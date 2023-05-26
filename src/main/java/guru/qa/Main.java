3f0b7d8f6020e74b6c83d9f72dd1285c52ffc8b0
Mikhail Ustinov <mikhailu359@gmail.com>
2023-05-26 20:20:01 +0200

    var added

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -2,6 +2,7 @@ package guru.qa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String message = "Hello world!";
+        System.out.println(message);
     }
 }
